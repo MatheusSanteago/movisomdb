e5c220af1b8e07155f81322f4358184c007c1cf6
Matheus <matheussanteago@gmail.com>
2022-10-08 02:13:22 -0300

    scripts

---
+++ app/main.js
@@ -0,0 +1,26 @@
+import { checkStorage, getMovie } from "../scripts/scripts.js";
+
+checkStorage();
+
+
+let verifyData = () => {
+
+    let searchTitle = document.querySelectorAll('input')[0].value;
+    let searchYear = document.querySelectorAll('input')[1].value;
+  
+    if(searchTitle){
+     
+        let data = {
+            title: searchTitle,
+            year: searchYear    
+        };
+      
+       getMovie(data);
+   
+        searchTitle = '';
+        searchYear = '';
+   }
+};
+
+let btn = document.querySelector('button');
+btn.addEventListener('click', verifyData);
---
+++ scripts/scripts.js
@@ -0,0 +1,74 @@
+let searched = [];
+
+export function checkStorage() {
+    let searchedLoad = JSON.parse(sessionStorage.getItem('movies')) || [];
+    searched = searchedLoad;
+    if(!searchedLoad == ''){    
+        console.log(searchedLoad);
+        searchedLoad.forEach(element => {
+            getData(element);
+        });
+    };
+}
+
+
+
+function getData(data){
+    let mainPage = document.querySelector('main');
+    mainPage.innerHTML += `
+    <div id="card">
+        <div id="card__info">
+        <h1>${data.Title}<h1>
+        <h3>${data.Year}<h3>
+        </div>
+        <img src="${data.Poster}">
+    </div>`
+};
+
+export function getMovie(data){
+    let url;
+    if(!data.year == ''){
+        console.log('Data escolhida');
+        url = `http://www.omdbapi.com/?t=${data.title}&y=${data.year}&apikey=b22b6a20`;
+    } else {
+        url = `http://www.omdbapi.com/?t=${data.title}&apikey=b22b6a20`;
+    }
+
+    fetch(url)
+    .then((res) => {
+        return res.json();
+    }).then((data) => {
+        if(data.Response == 'False'){
+            error(data.Error)
+        } else {
+            if(checkName(searched, data)) {
+
+            } else {
+                searched.push(data);
+                getData(data);
+                sessionStorage.setItem('movies', JSON.stringify(searched));
+            }
+        }
+    });
+}
+
+let error = (text) => {
+    let spanNav = document.querySelector('#spanError');
+    spanNav.innerHTML = '<i class="fa-solid fa-circle-exclamation"></i>  ' + text;
+    setTimeout(()=> {
+        spanNav.innerHTML = '';
+    },2000);
+}
+
+function checkName(arr,res){
+    let checked;
+
+    for (let i = 0; i < arr.length; i++) {
+        if(res.Title == arr[i].Title){
+            error("Already searched/Search another");
+            checked = true;
+        }
+    }
+
+    return checked;
+}
