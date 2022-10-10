d946ac8175a7af337d9eb8d8832506113af2fb09
Matheus <matheussanteago@gmail.com>
2022-10-09 22:20:44 -0300

    Add new functions

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-import { checkStorage, getMovie } from "../scripts/scripts.js";
+import { aside, AsideControl, checkStorage, getMovie  } from "../scripts/scripts.js";
 
 checkStorage();
-
 let verifyData = () => {
   let searchTitle = document.querySelectorAll("input")[0].value;
   let searchYear = document.querySelectorAll("input")[1].value;
@@ -18,3 +17,18 @@ let verifyData = () => {
 
 let btn = document.querySelector("button");
 btn.addEventListener("click", verifyData);
+
+export let fa = document.querySelectorAll("i");
+let exitIcon = document.querySelector(".fa-circle-xmark");
+let trashIcon = document.querySelector(".fa-trash");
+
+  fa[0].addEventListener('click', () => {
+    AsideControl(0);
+  });
+  exitIcon.addEventListener('click', () => {
+    AsideControl(1);
+  });
+  trashIcon.addEventListener('click', () => {
+    localStorage.removeItem('movies');
+    window.location.reload(false);
+  });
